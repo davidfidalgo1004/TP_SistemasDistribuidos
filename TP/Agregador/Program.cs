9076c3297c99a4b93f149606b060556998fcebc2
FABIO ANDRE DE ABREU PINTO <al78320@alunos.utad.pt>
2025-03-21 21:51:57 +0000

    wavy_agregador_server a comunicar

--- TP/Agregador/Program.cs
+++ TP/Agregador/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class AggregatorServer
+{
+    private static int port = 4000;
+    private static string serverIP = "127.0.0.1"; // IP do SERVIDOR
+    private static int serverPort = 6000;         //Porta do SERVIDOR
+
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("AGREGADOR pronto para receber conexões...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Thread clientThread = new Thread(() => HandleClient(client));
+            clientThread.Start();
+        }
+    }
+
+    static void HandleClient(TcpClient client)
+    {
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Recebido: {message}");
+
+                if (message.StartsWith("DADOS"))
+                {
+                    ForwardToServer(message);
+                }
+            }
+        }
+    }
+
+    static void ForwardToServer(string data)
+    {
+        try
+        {
+            using (TcpClient serverClient = new TcpClient(serverIP, serverPort))
+            using (NetworkStream stream = serverClient.GetStream())
+            {
+                byte[] messageBytes = Encoding.UTF8.GetBytes(data);
+                stream.Write(messageBytes, 0, messageBytes.Length);
+                Console.WriteLine($"Encaminhado para SERVIDOR: {data}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro ao encaminhar: {e.Message}");
+        }
+    }
+}
--- TP/Servidor/Program.cs
+++ TP/Servidor/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class DataServer
+{
+    private static int port = 6000;
+    private static string dataFile = "dados_oceanicos.txt";
+    private static object fileLock = new object(); // Mutex para controlo do acesso ao arquivo
+
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("SERVIDOR pronto para receber dados...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Thread clientThread = new Thread(() => HandleClient(client));
+            clientThread.Start();
+        }
+    }
+
+    static void HandleClient(TcpClient client)
+    {
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Recebido: {message}");
+
+                if (message.StartsWith("DADOS"))
+                {
+                    SaveData(message);
+                }
+            }
+        }
+    }
+
+    static void SaveData(string data)
+    {
+        lock (fileLock)
+        {
+            File.AppendAllText(dataFile, data + Environment.NewLine);
+            Console.WriteLine("Dados armazenados com sucesso.");
+        }
+    }
+}
